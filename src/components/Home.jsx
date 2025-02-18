54ece8a853da16be0956fce18401b731267aa759
abdullah107189 <abdullah107189@gmail.com>
2025-02-18 20:30:20 +0600

    get photo url from url under and return to the

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,16 @@ const Home = () => {
     const fetchingData = async () => {
       try {
         const { data } = await axios.get("https://pokeapi.co/api/v2/pokemon");
-        setPokeapi(data?.results);
+        const pokeapiData = await Promise.all(
+          data.results.map(async (p) => {
+            const { data } = await axios.get(p.url);
+            return {
+              name: p.name,
+              image: data?.sprites?.other?.["official-artwork"]?.front_default,
+            };
+          })
+        );
+        console.log("pokiapiData ,,,,", pokeapiData);
         console.log(data?.results);
       } catch (error) {
         console.log("Fetching problem, ", error.message);
