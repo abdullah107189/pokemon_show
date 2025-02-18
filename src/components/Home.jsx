5528a935ca08350c62c681fb771398205beb8f80
abdullah107189 <abdullah107189@gmail.com>
2025-02-18 20:23:01 +0600

    install axiso for fetcing data

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,20 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 const Home = () => {
   const [pokeapi, setPokeapi] = useState([]);
-  try {
-    useEffect(() => {
-      fetch("https://pokeapi.co/api/v2/pokemon")
-        .then((res) => res.json())
-        .then((data) => {
-          console.log(data);
-          setPokeapi(data?.results);
-          console.log(data?.results);
-        });
-    }, []);
-  } catch (error) {
-    console.log("Fetching problem, ", error.message);
-  }
+  useEffect(() => {
+    const fetchingData = async () => {
+      try {
+        const { data } = await axios.get("https://pokeapi.co/api/v2/pokemon");
+        setPokeapi(data?.results);
+        console.log(data?.results);
+      } catch (error) {
+        console.log("Fetching problem, ", error.message);
+      }
+    };
+    fetchingData();
+  }, []);
   return (
     <div>
       {pokeapi.map((p) => (
